76e3432e75ac9246c0a7c9531aabf1d978322fff
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-12-20 12:56:49 +0600

    Course Interface & Model Creation

---
+++ src/app/modules/Course/course.model.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from 'mongoose';
+import { TCourse, TPreRequisiteCourses } from './course.interface';
+
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
+  course: {
+    type: Schema.ObjectId,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const courseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  prefix: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  code: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  credits: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  preRequisiteCourses: [preRequisiteCoursesSchema],
+});
+
+export const Course = model<TCourse>('Course', courseSchema);
